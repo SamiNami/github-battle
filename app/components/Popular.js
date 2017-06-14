72a3a1d81631a4df0bd38f7d5df46f18c93f88b5
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-14 20:51:08 +0300

    refactored some code in popular

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,46 +1,54 @@
-var React = require("react");
+var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='languages'>
+      {languages.map((lang) => {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
-
-  constructor (props){
-    super(props);
+  constructor(props) {
+    super();
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: 'All',
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
-
-  updateLanguage(lang){
-    this.setState(function (){
-      return{
-        selectedLanguage: lang
-      };
-    })
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang,
+      }
+    });
   }
-
-
-  render(){
-
-    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
-
-    return(
-      <ul className = "languages">
-        {languages.map((lang) => {
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? {color: "#d0021b"} : null }
-              onClick={this.updateLanguage.bind(null, lang)}
-              key ={lang}>
-              {lang}
-            </li>
-          )
-        })}
-
-      </ul>
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+      </div>
     )
   }
 }
-
 
 module.exports = Popular;
